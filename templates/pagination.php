79b0675416bc23fcd8b92d61bd6f058c554760da
dgan89 <ilyha_roza@mail.ru>
2018-10-10 21:54:03 +0300

    Refactoring code

--- templates/pagination.php
+++ templates/pagination.php
@@ -1,9 +1,10 @@
 <?php
-    use razmik\helper\Html;
-    use razmik\helper\Url;
-    
-    $firstClass = $currentPage > 1 ? null : ' disabled';
-    $lastClass = $currentPage < $totalCount ? null : ' disabled';
+
+use razmik\helper\Html;
+use razmik\helper\Url;
+
+$firstClass = $currentPage > 1 ? null : ' disabled';
+$lastClass = $currentPage < $totalCount ? null : ' disabled';
 ?>
 
 <?php if ($totalCount > 1): ?>
@@ -13,13 +14,13 @@
                 <?= Html::a($firstChar, [$url, $queryParam => $currentPage - 1], ['class' => $defaultLinkClass]) ?>
             </li>
         <?php endif; ?>
-        
-        <?php for ($i = $start; $i <= $totalCount; $i ++): ?>
+
+        <?php for ($i = $start; $i <= $totalCount; $i++): ?>
             <li class="<?= $defaultItemClass . ($currentPage == $i ? ' active' : null) ?>">
                 <?= Html::a($i, [$url, $queryParam => $i], ['class' => $defaultLinkClass]) ?>
             </li>
         <?php endfor; ?>
-        
+
         <?php if ($showDisabled || !$lastClass): ?>
             <li class="<?= $defaultItemClass . $lastClass ?>">
                 <?= Html::a($lastChar, [$url, $queryParam => $currentPage + 1], ['class' => $defaultLinkClass]) ?>
